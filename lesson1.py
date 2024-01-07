18c783a24d7587084dc43cd1c3a06407473ee2b9
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2024-01-07 00:44:23 -0500

    little updates

--- lesson1.py
+++ lesson1.py
@@ -50,4 +50,40 @@ max_logins_type = type(max_logins)
 # Display `max_logins_type`
 print(max_logins_type)
 
+# Assign `login_attempts` to the value 2
+login_attempts = 2
+# Assign `login_attempts_type` to the data type of `login_attempts`
+login_attempts_type = type(login_attempts)
+# Display `login_attempts_type`
+print(login_attempts_type)
 
+# Assign `max_logins` to the value 3
+max_logins = 3
+# Assign `login_attempts` to the value 2
+login_attempts = 2
+# Determine whether the current number of login attempts a user has made is less than or equal to the maximum number of login attempts allowed,
+# and display the resulting Boolean value
+check_login_attempts = login_attempts <= max_logins
+print(check_login_attempts)
+
+# Assign `max_logins` to the value 3
+max_logins = 3
+# Assign `login_attempts` to a specific value
+login_attempts = 6
+# Determine whether the current number of login attempts a user has made is less than or equal to the maximum number of login attempts allowed,
+# and display the resulting Boolean value
+print(login_attempts <= max_logins)
+
+# Assign `login_status` to the Boolean value False
+login_status = False
+# Assign `login_status_type` to the data type of `login_status`
+login_status_type = type(login_status)
+# Display `login_status_type`
+print(login_status_type)
+
+# WRITING CONDITIONAL STATEMENTS
+operating_system = "0S 3"
+if operating_system == "0S 2":
+    print("Updates needed")
+else:
+    print ("No updates needed" )
