1205c73a263e415d424d7e98ab50753d74c60e21
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2024-01-06 22:00:43 -0500

    interesting synthax

--- lesson1.py
+++ lesson1.py
@@ -1,10 +1,53 @@
 # simple use of boolean
 print(10 < 5)
 print(6 > 5)
+
 # Printing a list in python
 print(["adnan", "lili", "tim", "bryson", "pak"])
-my_date_of_birth = "11th July 1991"
+my_date_of_birth = "6th march 1957"
 print(my_date_of_birth)
+
 # use the type function
 data_type = type(my_date_of_birth)
 print(data_type)
+
+# from lab on googel cert
+# Assign the `device_id` variable to the device ID that only specified users can access
+device_id = "72e08x0"
+# Display `device_id`
+print(device_id)
+
+# Assign the `device_id` variable to the device ID that only specified users can access
+device_id = "72e08x0"
+# Assign `device_id_type` to the data type of `device_id`
+device_id_type = type(device_id)
+# Display `device_id_type`
+print(device_id_type)
+
+# Assign `username_list` to the list of usernames who are allowed to access the device
+username_list = ["madebowa", "jnguyen", "tbecker", "nhersh", "redwards"]
+# Display `username_list`
+print(username_list)
+
+# Assign `username_list_type` to the data type of `username_list`
+username_list_type = type(username_list)
+# Display `username_list_type`
+print(username_list_type)
+
+# Assign `username_list` to the list of usernames who are allowed to access the device
+username_list = ["madebowa", "jnguyen", "tbecker", "nhersh", "redwards"]
+# Display `username_list`
+print(username_list)
+# Assign `username_list` to the updated list of usernames who are allowed to access the device
+username_list = ["madebowa", "jnguyen", "tbecker", "nhersh", "redwards", "lpope"]
+# Display `username_list`
+print(username_list)
+
+# Assign `max_logins` to the value 3
+max_logins = 3
+# Assign `max_logins_type` to the data type of `max_logins`
+max_logins_type = type(max_logins)
+# Display `max_logins_type`
+print(max_logins_type)
+
+
