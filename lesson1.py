ad5acfe29b128ccb077c377afac855b6b783a44b
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2024-01-06 21:21:04 -0500

    just got started

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+# simple use of boolean
+print(10 < 5)
+print(6 > 5)
+# Printing a list in python
+print(["adnan", "lili", "tim", "bryson", "pak"])
+my_date_of_birth = "11th July 1991"
+print(my_date_of_birth)
+# use the type function
+data_type = type(my_date_of_birth)
+print(data_type)
