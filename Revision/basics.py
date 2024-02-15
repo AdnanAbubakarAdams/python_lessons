dfe61488a6b0642cbc8cbb103ad02c23ea6cb0bc
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2024-02-15 17:43:08 -0500

    keepitgoing

--- Revision/basics.py
+++ Revision/basics.py
@@ -9,3 +9,7 @@ print(type(ghana_independece))
 favorite_united_players = ["rooney", "degea", "scholes", "ronaldo", "vannistelrooy", "chicharito", "giggs", "vandersar"]
 print("my favorite united Players of all time are:", favorite_united_players)
 print(type(favorite_united_players))
+
+max_logins = 3
+print(max_logins)
+print(type(max_logins))
