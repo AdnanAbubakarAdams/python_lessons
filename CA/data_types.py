ec5bd451b56add7b3f2df3463ee2f0eb7d39554e
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2024-06-01 00:10:26 -0400

    netplus

---
+++ CA/data_types.py
@@ -0,0 +1,13 @@
+# Data types
+# Numeric types => Number with or without decimals => int, float, num = 5, complex
+# Text type => string of characters => str = 'this is a string'
+# Boolean type => Continue = True, should_not_count = False
+
+the_distance_from_ny_to_accra = 5123
+print(type(the_distance_from_ny_to_accra)) # int
+
+i_bench_225_lbs = True
+print(type(i_bench_225_lbs)) # bool
+
+candy_price = 6.55
+print(type(candy_price))
---
+++ CA/terminal.py
@@ -0,0 +1,35 @@
+# we give out commands to the computer by writing them in text file using programming language. These files are called programs. Running a program means telling a computer to read a text file, translate it to the set of operations that it understands, and perform those actions.
+my_name = 'rondo'
+print('Hello and welcome ' + my_name + '!')
+
+# working with numbers
+x = 10
+print(x) # 10
+print(type(x)) # int
+
+y = 11.01
+print(y) # 11.01
+print(type(y)) # float
+
+a = True
+print(a) # True
+print(type(a)) # bool
+
+x = 'This is a string'
+print(x) # This is a string
+print(type(x)) # str
+
+# working with inputs
+name = input("chicharito")
+print(name)
+
+enter = str(input('What is your name?: '))
+print('welcome to united states of America', enter)
+print(type(enter))
+
+lucky_number = int(input('Enter a number: '))
+print('Your lucky number is:', lucky_number)
+print(type(lucky_number))
+
+john_age = 12
+print('john is ', str(john_age))
